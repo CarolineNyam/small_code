d96bd72b2f931ea6cab338cfdf8c7aa4cc2a8ced
CarolineNyam <caroline.nyamupanda@mycit.ie>
2023-06-07 08:43:02 +0100

    Initial commit

---
+++ SEP = 'SOME CHARACTERS' lab 1.py
@@ -0,0 +1,7 @@
+# Caroline
+
+item1 = 'soup'
+item2 = 'bread'
+item3 = 'cheese'
+
+print(item1, item2, item3, sep='---')
---
+++ main.py
@@ -0,0 +1,8 @@
+# Author: Caroline
+# Create a script t
+
+animal1 = 'cat'
+animal2 = 'dog'
+animal3 = 'pig'
+
+print(animal1, animal2, animal3, end='!')
